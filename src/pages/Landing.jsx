69f55cf3bfaa8001f0d6691bfc8bdcd435b66711
Soffi Zahir <soffizahir2016@gmail.com>
2024-01-09 11:53:23 -0500

    Fetch data from API using axios

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,27 @@
+import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailList";
+const cocktailSearchUrl =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-export const loader = () => {
-  return "something ";
+export const loader = async () => {
+  const searchTerm = "";
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { meals: response.data.meals, searchTerm };
 };
 function Landing() {
-  const data = useLoaderData();
-  console.log(data);
-  return <div>Landing</div>;
+  const { searchTerm, meals } = useLoaderData();
+
+  return (
+    <>
+      <h2>{searchTerm}</h2>
+      <Wrapper>
+        {meals.map((meal) => {
+          return <li key={meal.idMeal}>{meal.strMeal}</li>;
+        })}
+      </Wrapper>
+    </>
+  );
 }
 
 export default Landing;
