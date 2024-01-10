796645f45af913b5f9efed3fc7efa494f1ccdc47
Soffi Zahir <soffizahir2016@gmail.com>
2024-01-10 15:52:56 -0500

    Search Form - Setup

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SearchForm";
+import { Form, useNavigation } from "react-router-dom";
 function SearchForm() {
-  return <Wrapper>SearchForm</Wrapper>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue="pizza"
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "searching..." : "search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 }
 
 export default SearchForm;
